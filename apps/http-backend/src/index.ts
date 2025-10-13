f328e5537ee005e866bccd8cfb42b81eccf0fd1a
woustachemax <sid2011thakkar@gmail.com>
2025-10-13 15:08:49 -0700

    railway-deployment-issues-fixed

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use('/api/v1/', router)
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
     console.log(`http backend active on port: ${PORT}`);
